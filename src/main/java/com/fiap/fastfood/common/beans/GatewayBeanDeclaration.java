325ca0d2ae1011c3ee12e00df5ed82db5b16b761
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2024-08-18 21:02:17 -0300

    feature: cria bean do orquestrationGateway

--- src/main/java/com/fiap/fastfood/common/beans/GatewayBeanDeclaration.java
+++ src/main/java/com/fiap/fastfood/common/beans/GatewayBeanDeclaration.java
@@ -1,7 +1,11 @@
 package com.fiap.fastfood.common.beans;
 
 import com.fiap.fastfood.common.interfaces.datasources.PaymentRepository;
+import com.fiap.fastfood.common.interfaces.external.MessageSender;
+import com.fiap.fastfood.common.interfaces.gateways.OrquestrationGateway;
 import com.fiap.fastfood.common.interfaces.gateways.PaymentGateway;
+import com.fiap.fastfood.common.interfaces.usecase.PaymentUseCase;
+import com.fiap.fastfood.communication.gateways.OrquestrationGatewayImpl;
 import com.fiap.fastfood.communication.gateways.PaymentGatewayImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +16,10 @@ public class GatewayBeanDeclaration {
     @Bean
     public PaymentGateway paymentGateway(PaymentRepository repository) {
         return new PaymentGatewayImpl(repository);
+    }
+
+    @Bean
+    public OrquestrationGateway orquestrationGateway(PaymentUseCase paymentUseCase, PaymentGateway paymentGateway, MessageSender messageSender) {
+        return new OrquestrationGatewayImpl(paymentUseCase, paymentGateway, messageSender);
     }
 }
